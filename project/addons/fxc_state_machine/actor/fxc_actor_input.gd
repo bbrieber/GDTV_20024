caf66efd408c972b2563a8b8f26b79bb72eb6f14
Benjamin Brieber <bbrieber@users.noreply.github.com>
2024-06-12 23:44:22 +0200

    update input handling

--- project/addons/fxc_state_machine/actor/fxc_actor_input.gd
+++ project/addons/fxc_state_machine/actor/fxc_actor_input.gd
@@ -5,5 +5,17 @@ class_name FxcActorInput extends Node
 func get_axis_value(axis_name:StringName)->float:
 	return 0.0
 
-func is_action_requested(action_name:StringName)->bool:
+
+
+func get_direction(input_name:StringName)->Vector2:
+	return Vector2.ZERO
+
+func start_action_requested(action_name:StringName)->bool:
+	return false
+
+
+func stop_action_requested(action_name:StringName)->bool:
+	return false
+
+func is_action_active(action_name:StringName)->bool:
 	return false
